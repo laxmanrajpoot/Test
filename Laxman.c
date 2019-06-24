3366e9a5715027a9b7f6674e2073668d95024a18
laxmanrajpoot <laxman.gla_bca17@gla.ac.in>
2019-06-24 16:30:19 +0530

    init commit

---
+++ Laxman.c
@@ -0,0 +1,5 @@
+//
+// Created by Laxman rajpoot on 24-Jun-19.
+//
+
+#include "Laxman.h"
---
+++ Laxman.h
@@ -0,0 +1,8 @@
+//
+// Created by Laxman rajpoot on 24-Jun-19.
+//
+
+#ifndef TEST_LAXMAN_H
+#define TEST_LAXMAN_H
+
+#endif //TEST_LAXMAN_H
